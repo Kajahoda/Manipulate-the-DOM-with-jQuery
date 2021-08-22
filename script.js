9f4c20d18300491e7007ee964b8eecd43635d5ae
Kajahoda <karlicekhav@gmail.com>
2021-08-22 16:31:46 +0200

    2nd exercise

--- script.js
+++ script.js
@@ -4,4 +4,9 @@ $('.list').append('<li>'+names[1]+'</li>');
 $('.list').append('<li>'+names[2]+'</li>');
 $('.list').append('<li>'+names[3]+'</li>');
 
-
+let additionalBlock = {
+    title: "Added with javascript",
+    text: "This block was added using JavaScript's jQuery library. How awesome!"
+  };
+  $('body').append('<heading>'+additionalBlock.title+'</heading>');
+  $('body').append('<p>'+additionalBlock.text+'</p>');
