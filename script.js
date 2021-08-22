23d0f41e09092f84acfc0eab4b3a3aaf2afae040
Kajahoda <karlicekhav@gmail.com>
2021-08-22 16:24:57 +0200

    1st exercise

---
+++ script.js
@@ -0,0 +1,7 @@
+names = ['Kája', 'Dan', 'Bohuslav', 'Břetislav'];
+$('.list').append('<li><strong>'+names[0]+'</strong></li>');
+$('.list').append('<li>'+names[1]+'</li>');
+$('.list').append('<li>'+names[2]+'</li>');
+$('.list').append('<li>'+names[3]+'</li>');
+
+
